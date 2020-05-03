4aaaa3d6759f06f835fc729a6de7485470ee911c
Jamshid Massomy <52878597+JamshidMassomy@users.noreply.github.com>
2020-05-03 21:43:40 +0300

    Update CROSconfig.java

--- src/main/java/sys/auth/configuration/CROSconfig.java
+++ src/main/java/sys/auth/configuration/CROSconfig.java
@@ -17,14 +17,4 @@ public class CROSconfig   {
             }
         };
     }
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/api/**")
-//                .allowedOrigins("http://domain2.com")
-//                .allowedMethods("PUT", "DELETE")
-//                .allowedHeaders("header1", "header2", "header3")
-//                .exposedHeaders("header1", "header2")
-//                .allowCredentials(false).maxAge(3600);
-
-
 }
